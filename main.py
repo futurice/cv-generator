34f5bd02a8dcbc825c2e21242930b25c2c366ad5
Daniel Landau <daniel@landau.fi>
2016-02-18 11:38:30 +0200

    make configurable by input

--- main.py
+++ main.py
@@ -2,64 +2,24 @@
 
 import cherrypy
 import weasyprint
+from jinja2 import Template, FileSystemLoader, Environment
 
-class Pdf(object):
-    def index(self):
-        cherrypy.response.headers['Content-Type'] = 'application/pdf'
-        doc = weasyprint.HTML(base_url=".", string = """
-        <!doctype html>
-        <html>
-          <head>
-        <title>CV</title>
-          </head>
-          <body>
-            <div class="left-pane">
-              <img class="pic" src="foo.jpg" />
-              <div class="keywords">
-                <ul>
-                  <li>Web Backend</li>
-                  <li>Web Frontend</li>
-                  <li>DevOps</li>
-                  <li>Software Project Management</li>
-                </ul>
-              </div>
-            </div>
-            <div class="right-pane">
-              <h1 class="name">Daniel Landau</h1>
-              <h2 class="title">Software Developer</h2>
-              <p class="intro-text">I am a builder and problem solver. The solution will get done in whatever way is required from Haskell through Java to machine language and beyond. I understand the language of business and can ask the right questions at the right time to arrive at the best possible outcome. I am a generalist with strong experience in full stack web development.</p>
-              <div class="experience">
-                <h3>Experience</h3>
-                <dl>
-                <dt>Software Developer</dt>
-                <dd>Futurice Oy 2014-Present</dd>
-                <span>—</span>
-                <dt>High Performance Analysis</dt>
-                <dd>Cray Inc 2013-2014</dd>
-                <span>—</span>
-                <dt>Research Assistant</dt>
-                <dd>University of Helsinki 2011-2012</dd>
-                <span>—</span>
-                <dt>Intern</dt>
-                <dd>CSC 2011, FMI 2009-2011</dd>
-                </dl>
-              </div>
-              <div class="education">
-                <h3>Education</h3>
-                <dl>
-                <dt>University of Helsinki</dt>
-                <dd>M.Sc., Theoretical Physics</dd>
-                <dd>2008-2013</dd>
-                </dl>
-              </div>
-              <div class="social">
-                <span class="email">daniel.landau@futurice.com</span>
-                <span class="twitter">@Daniel_Landau</span>
-                <span>fi.linkedin.com/in/landaudaniel</span>
-              </div>
-            </div>
-          </body>
-        </html>""")
+class App(object):
+    @cherrypy.expose('cv')
+    @cherrypy.tools.json_in()
+    def cv(self, **kwargs):
+        loader = FileSystemLoader('templates')
+        templateEnv = Environment(loader =  loader)
+        data = cherrypy.request.json
+        isPng = kwargs['type'] == 'png'
+        print(data['keywords'])
+        if (isPng):
+            responseType = 'application/png'
+        else:
+            responseType = 'application/pdf'
+
+        cherrypy.response.headers['Content-Type'] = responseType
+        template = templateEnv.get_template('cv.html')
         style = weasyprint.CSS(string = """
         .pic {
           position: absolute;
@@ -165,7 +125,11 @@ class Pdf(object):
           margin: 0;
         }
         """)
-        return doc.write_png(target=None, stylesheets=[style])
-    index.exposed = True
+        doc = weasyprint.HTML(base_url=".", string = template.render(data))
+        if isPng:
+            return doc.write_png(target=None, stylesheets=[style])
+        else:
+            return doc.write_pdf(target=None, stylesheets=[style])
+    # cv.exposed = True
 
-cherrypy.quickstart(Pdf())
+cherrypy.quickstart(App())
